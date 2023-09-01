dd3b20e653fc430e6a526ddf4875d6564c81ae01
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2023-09-01 09:57:53 +0200

    Update contact.component.ts

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ContactService} from "src/app/shared/contact.service";
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
+  contactForm: FormGroup | any;
+  constructor(private fb: FormBuilder,private contactService: ContactService) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.contactForm = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(5)]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required]],
+    });
+  }
+
+  saveContactDetails() {
+    this.contactService.createData(this.contactForm.value).subscribe(res => {
+      this.contactForm.reset();
+      Swal.fire({
+        title: 'Success',
+        text: 'Your message has been sent successfully',
+        icon: 'success',
+        confirmButtonText: 'Ok'
+      }).then(r =>
+        window.location.reload());
+    } , error => {
+      console.log(error);
+      Swal.fire({
+        title: 'Error',
+        text: 'Something went wrong, please try again later',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      }).then(r =>
+        window.location.reload());
+    });
+  }
 
 }
