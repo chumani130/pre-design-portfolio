e983ae805ea99e7ae2a50ad16ca28610ced087aa
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2023-09-11 13:13:18 +0200

    Update navbar.component.ts

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,10 +6,12 @@ import { Router } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   isNavbarActive = false;
 
   constructor(private router: Router) {}
+  ngOnInit(): void {
+  }
 
   toggleNavbar() {
     this.isNavbarActive = !this.isNavbarActive;
@@ -19,6 +21,28 @@ export class NavbarComponent {
     if (this.isNavbarActive) {
       this.isNavbarActive = false;
     }
+  }
+  scrollToHome() {
+    document.getElementById("home")?.scrollIntoView({behavior: "smooth"});
+
+  }
+  scrollToAbout() {
+    document.getElementById("about")?.scrollIntoView();
+  }
+  scrollToSkills() {
+    document.getElementById("skills")?.scrollIntoView();
+  }
+  scrollToProjects() {
+    document.getElementById("projects")?.scrollIntoView();
+  }
+  scrollToEducation() {
+    document.getElementById("education")?.scrollIntoView();
+  }
+  scrollToExperience() {
+    document.getElementById("experience")?.scrollIntoView();
+  }
+  scrollToContact() {
+    document.getElementById("contact")?.scrollIntoView();
   }
 
 }
