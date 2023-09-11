e08290e49727505d5503048e0bd8310dd1365799
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2023-09-11 13:13:24 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Routes } from '@angular/router';
 
 
@@ -7,6 +7,35 @@ import { Routes } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pre-design-portfolio';
+
+  constructor(){ }
+  ngOnInit(): void {
+    
+  }
+  scrollToHome() {
+    document.getElementById("home")?.scrollIntoView();
+    const homeId =  document.getElementById("home")?.scrollIntoView();
+    console.log(homeId)
+  }
+  scrollToAbout() {
+    document.getElementById("about")?.scrollIntoView();
+  }
+  scrollToSkills() {
+    document.getElementById("skills")?.scrollIntoView();
+  }
+  scrollToProjects() {
+    document.getElementById("projects")?.scrollIntoView();
+  }
+  scrollToEducation() {
+    document.getElementById("education")?.scrollIntoView();
+  }
+  scrollToExperience() {
+    document.getElementById("experience")?.scrollIntoView();
+  }
+  scrollToContact() {
+    document.getElementById("contact")?.scrollIntoView();
+  }
+  
 }
